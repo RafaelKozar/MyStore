97edb0a645438ac5cdd2e2d70d5901c01d3e6827
Rafael Kozar <rafael.kozar@alta-rt.com>
2022-05-02 15:59:54 -0300

    auto mapper

---
+++ src/MyStore.Catalago.Application/AutoMapper/DomainToDtoMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MyStore.Catalago.Application.ViewModels;
+using MyStore.Catalago.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Catalago.Application.AutoMapper
+{
+    public class DomainToDtoMappingProfile : Profile
+    {
+        public DomainToDtoMappingProfile()
+        {
+            CreateMap<Produto, ProdutoDto>()
+                .ForMember(d => d.Largura, o => o.MapFrom(s => s.Dimensoes.Largura))
+                .ForMember(d => d.Altura, o => o.MapFrom(s => s.Dimensoes.Altura))
+                .ForMember(d => d.Profundidade, o => o.MapFrom( s => s.Dimensoes.Profundidade));
+            CreateMap<Categoria, CategoriaDto>();
+        }
+    }
+}
---
+++ src/MyStore.Catalago.Application/AutoMapper/DtoToDomainMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MyStore.Catalago.Application.ViewModels;
+using MyStore.Catalago.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Catalago.Application.AutoMapper
+{
+    public class DtoToDomainMappingProfile : Profile
+    {
+        public DtoToDomainMappingProfile()
+        {
+            CreateMap<ProdutoDto, Produto>()
+                .ConstructUsing(p =>
+                    new Produto(p.Nome, p.Descricao, p.Ativo,
+                        p.Valor, p.CategoriaId, p.DataCadastro,
+                        p.Imagem, new Dimensoes(p.Altura, p.Largura, p.Profundidade)));
+
+            CreateMap<CategoriaDto, Categoria>()
+                .ConstructUsing(c => new Categoria(c.Nome, c.Codigo));
+        }
+    }
+}
