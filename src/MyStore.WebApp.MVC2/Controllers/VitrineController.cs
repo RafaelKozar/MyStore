f82ee75546599b5fa0210dc85df0a11809d79792
Rafael Kozar <rafael.kozar@alta-rt.com>
2022-10-26 15:25:10 -0300

    Vitrine

---
+++ src/MyStore.WebApp.MVC2/Controllers/VitrineController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyStore.Catalago.Application.ViewModels;
+using RestSharp;
+
+namespace MyStore.WebApp.MVC2.Controllers
+{
+    public class VitrineController : Controller
+    {   
+        private readonly RestClient _restClient;
+
+        public VitrineController(RestClient res)
+        {            
+            _restClient = res;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Route("vitrine")]
+        public async Task<IActionResult> Index()
+        {
+            var request = new RestRequest("Produto", Method.Get);
+            var result = await _restClient.GetAsync<IEnumerable<ProdutoDto>>(request);
+            return View(result);
+            //return View(await _produtoAppService.ObterTodos());
+        }
+
+        [HttpGet]
+        [Route("produto-detalhe/{id}")]
+        public async Task<IActionResult> ProdutoDetalhe(Guid id)
+        {
+            var request = new RestRequest($"Produto/{id.ToString()}", Method.Get);
+            var result = await _restClient.GetAsync<ProdutoDto>(request);
+            return View(result);
+            //return View(await _produtoAppService.ObterPorId(id));
+        }
+    }
+}
--- src/MyStore.WebApp.MVC2/Program.cs
+++ src/MyStore.WebApp.MVC2/Program.cs
@@ -40,6 +40,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Vitrine}/{action=Index}/{id?}");
 
 app.Run();
