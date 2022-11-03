96f8d838a23b657c0d22dd6547ef3ccf48798384
Rafael Kozar <rafael.kozar@alta-rt.com>
2022-11-03 16:49:20 -0300

    setup vendas

---
+++ src/MyStore.Vendas.Domain/IPedidoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Vendas.Domain
+{
+    internal interface IPedidoRepository
+    {
+    }
+}
---
+++ src/MyStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,13 @@
+﻿using MyStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyStore.Vendas.Domain
+{
+    public class Pedido : Entity, IAggregateRoot
+    {
+    }
+}
